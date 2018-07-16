7dda64e775295ccc926aaf6271133a825570ceb1
Sean Oughton <seanoughton@gmail.com>
2018-07-16 23:25:26 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,8 @@ class ProductsController < ApplicationController
 
   def data
     product = Product.find(params[:id])
-    render json: ProductSerializer.serialize(product)
+    #render json: ProductSerializer.serialize(product)
+    render json: product.to_json
   end
 
   private
